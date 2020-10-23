1e4b3619a60fc64c9700134daf0291ef7ffcee39
Vladimir <vzlobin2003@yandex.ru>
2020-10-23 16:27:30 +0300

    refactor RepositoryTypeService

--- src/main/java/com/vz89/hometask/service/RepositoryTypeService.java
+++ src/main/java/com/vz89/hometask/service/RepositoryTypeService.java
@@ -3,16 +3,44 @@ package com.vz89.hometask.service;
 import com.vz89.hometask.repository.AccountRepository;
 import com.vz89.hometask.repository.DeveloperRepository;
 import com.vz89.hometask.repository.SkillRepository;
+import com.vz89.hometask.repository.db.DbAccountRepositoryImpl;
+import com.vz89.hometask.repository.db.DbDeveloperRepositoryImpl;
+import com.vz89.hometask.repository.db.DbSkillRepositoryImpl;
+import com.vz89.hometask.repository.io.IoAccountRepositoryImpl;
+import com.vz89.hometask.repository.io.IoDeveloperRepositoryImpl;
+import com.vz89.hometask.repository.io.IoSkillRepositoryImpl;
+import com.vz89.hometask.repository.json.JsonAccountRepositoryImpl;
+import com.vz89.hometask.repository.json.JsonDeveloperRepositoryImpl;
+import com.vz89.hometask.repository.json.JsonSkillRepositoryImpl;
 
 public class RepositoryTypeService {
     private static AccountRepository accountRepository;
     private static DeveloperRepository developerRepository;
     private static SkillRepository skillRepository;
 
-    public static void setRepositoryType(AccountRepository accountRepository, DeveloperRepository developerRepository, SkillRepository skillRepository) {
-        RepositoryTypeService.accountRepository = accountRepository;
-        RepositoryTypeService.developerRepository = developerRepository;
-        RepositoryTypeService.skillRepository = skillRepository;
+    public static void setRepositoryType(String type) {
+        switch (type) {
+            case ("db"):
+                RepositoryTypeService.accountRepository = new DbAccountRepositoryImpl();
+                RepositoryTypeService.developerRepository = new DbDeveloperRepositoryImpl();
+                RepositoryTypeService.skillRepository = new DbSkillRepositoryImpl();
+                break;
+            case ("json"):
+                RepositoryTypeService.accountRepository = new JsonAccountRepositoryImpl();
+                RepositoryTypeService.developerRepository = new JsonDeveloperRepositoryImpl();
+                RepositoryTypeService.skillRepository = new JsonSkillRepositoryImpl();
+                break;
+            case ("io"):
+                RepositoryTypeService.accountRepository = new IoAccountRepositoryImpl();
+                RepositoryTypeService.developerRepository = new IoDeveloperRepositoryImpl();
+                RepositoryTypeService.skillRepository = new IoSkillRepositoryImpl();
+                break;
+            default:
+                RepositoryTypeService.accountRepository = new DbAccountRepositoryImpl();
+                RepositoryTypeService.developerRepository = new DbDeveloperRepositoryImpl();
+                RepositoryTypeService.skillRepository = new DbSkillRepositoryImpl();
+                break;
+        }
     }
 
     public static AccountRepository getAccountRepoType() {
--- src/main/java/com/vz89/hometask/view/RunnerView.java
+++ src/main/java/com/vz89/hometask/view/RunnerView.java
@@ -1,14 +1,5 @@
 package com.vz89.hometask.view;
 
-import com.vz89.hometask.repository.db.DbAccountRepositoryImpl;
-import com.vz89.hometask.repository.db.DbDeveloperRepositoryImpl;
-import com.vz89.hometask.repository.db.DbSkillRepositoryImpl;
-import com.vz89.hometask.repository.io.IoAccountRepositoryImpl;
-import com.vz89.hometask.repository.io.IoDeveloperRepositoryImpl;
-import com.vz89.hometask.repository.io.IoSkillRepositoryImpl;
-import com.vz89.hometask.repository.json.JsonAccountRepositoryImpl;
-import com.vz89.hometask.repository.json.JsonDeveloperRepositoryImpl;
-import com.vz89.hometask.repository.json.JsonSkillRepositoryImpl;
 import com.vz89.hometask.service.IOService;
 import com.vz89.hometask.service.RepositoryTypeService;
 
@@ -17,15 +8,7 @@ public class RunnerView {
     public void start() {
         System.out.println("Введите тип источника данных, с которым будете работать: db,json,io");
         String entity = IOService.read();
-        if (entity.equals("db")) {
-            RepositoryTypeService.setRepositoryType(new DbAccountRepositoryImpl(),new DbDeveloperRepositoryImpl(), new DbSkillRepositoryImpl());
-        }
-        if (entity.equals("json")) {
-            RepositoryTypeService.setRepositoryType(new JsonAccountRepositoryImpl(),new JsonDeveloperRepositoryImpl(), new JsonSkillRepositoryImpl());
-        }
-        if (entity.equals("io")) {
-            RepositoryTypeService.setRepositoryType(new IoAccountRepositoryImpl(),new IoDeveloperRepositoryImpl(), new IoSkillRepositoryImpl());
-        }
+        RepositoryTypeService.setRepositoryType(entity);
         run();
     }
 
